56565097ef6b445f1d3ea4e71665827c15522b85
avadhutap459 <avadhutap459@gmail.com>
2025-10-31 07:11:16 +0530

    added testing freamwork

---
+++ WebApi.Test/ValuesControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+
+namespace WebApi.Test
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsTwoValues()
+        {
+            // Arrange
+            var controller = new ValuesController();
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+            Assert.Contains("value1", result);
+            Assert.Contains("value2", result);
+        }
+    }
+}
