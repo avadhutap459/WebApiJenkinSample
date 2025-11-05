1072d616fbf794a78f548d614e59de1f86ec7848
avadhutap459 <avadhutap459@gmail.com>
2025-11-05 16:41:16 +0530

    updated

--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace WebApi.Controllers
             return Ok(info);
         }
 
-        [HttpGet("GetDotnetInfo")]
+        [HttpGet("GetDotInfo")]
         public IActionResult GetDotnetInfo()
         {
             var info = new
