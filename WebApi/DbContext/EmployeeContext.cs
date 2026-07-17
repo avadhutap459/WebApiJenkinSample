81e46ab3407ffd5def33f269956877bae45af5d1
avadhutap459 <avadhutap459@gmail.com>
2026-07-17 09:26:55 +0530

    added

---
+++ WebApi/DbContext/EmployeeContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApi.Models;
+
+namespace WebApi._DbContext
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) // Specify the generic type for DbContextOptions
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Uncle",
+                LastName = "Bob",
+                Email = "uncle.bob@gmail.com",
+                DateOfBirth = new DateTime(1979, 04, 25),
+                PhoneNumber = "999-888-7777"
+            }, new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Jan",
+                LastName = "Kirsten",
+                Email = "jan.kirsten@gmail.com",
+                DateOfBirth = new DateTime(1981, 07, 13),
+                PhoneNumber = "111-222-3333"
+            });
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi._DbContext;
+using WebApi.Models;
+using WebApi.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<EmployeeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IDataRepository<Employee>, EmployeeManager>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
